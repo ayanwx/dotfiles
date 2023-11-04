aca74133008139c0a62f0de63eb3446aa67ca5eb
ayanwx <ayanxdev@gmail.com>
2023-11-04 18:19:06 +0600

    ..

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -1,14 +1,14 @@
 fish_add_path /home/ayanw/.cargo/bin
 fish_add_path /home/ayanw/.yarn/bin
 
-set -x EDITOR "code"
+set -x EDITOR   "code"
 set -x FISHCONF "$HOME/.config/fish"
 set -x NVIMCONF "$HOME/.config/nvim"
+set -x EWWCONF  "$HOME/.config/nvim"
 
 alias nv="neovide $argv"
 alias ls="exa --icons $argv"
 alias tree="exa --icons --tree $argv"
 alias cat="bat --style plain --paging never --theme Dracula $argv"
 alias du="dust $argv"
-alias tlauncher="java -jar /bin/tlauncher $argv"
 alias img="kitty +kitten icat $argv"
